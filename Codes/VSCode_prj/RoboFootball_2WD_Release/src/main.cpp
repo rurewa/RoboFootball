f67e70503e538fe7c4e6699e912e533b8ce7a95f
rurewa <rurewa@gmail.com>
2021-06-19 09:18:01 +0300

    Update VSCode_prj/RoboFootball_2WD_Release/src/main.cpp: fix

--- Codes/VSCode_prj/RoboFootball_2WD_Release/src/main.cpp
+++ Codes/VSCode_prj/RoboFootball_2WD_Release/src/main.cpp
@@ -16,25 +16,25 @@ const int PS2_CMD = 11;
 const int PS2_SEL = 10;
 const int PS2_CLK = 13;
 
-//const bool pressures = false;
-//const bool pressures = true; // Аналоговое управление кнопками
-//const bool rumble = false;
-//const bool rumble = true; // Вибрация контроллера PS2
-
 PS2X ps2x; // Класс PS2 контроллера
 
 const int actuator = 5; // Соленоид
-//int error = 0;
+int error = 0;
+//byte type = 0;
 byte vibrate = 0;
-const int SPEED_PWM = 40;
-QGPMaker_MotorShield AFMS = QGPMaker_MotorShield(); // Объект шины I2C
+
+//int lx = 0;
+//int ly =  0;
+
+// Объект шины I2C
+QGPMaker_MotorShield AFMS = QGPMaker_MotorShield();
 // Объекты моторов (M3) или (M1) на шилде
 QGPMaker_DCMotor  *DCMotor_3 = AFMS.getMotor(3); // Правая сторона
 QGPMaker_DCMotor  *DCMotor_1 = AFMS.getMotor(1); // Левая сторона
 
 void setup() {
-  Serial.begin(9600); // Монитор порта
-  AFMS.begin(50); // Инициализация библиотеки
+  Serial.begin(9600); // Инициализация библиотеки
+  AFMS.begin(50);
   Serial.println("===========beging==========");
   // Диагностика контроллера PS2X
   int error = 0;
@@ -44,14 +44,13 @@ void setup() {
     else { delay(100); }
   }
   while (true);
-  AFMS.begin(50);  // Задаём частоту.
-  pinMode(actuator, OUTPUT); // Соленоид
+  AFMS.begin(50);  // Задаём частоту. По умолчанию 1.6KHz
+  pinMode(actuator, OUTPUT);
 }
 
 void loop() {
-  ps2x.read_gamepad(false, vibrate); // Читаем контроллер и устанавливаем вибро-мотор на
-  // вращение с «вибрирующей» скоростью
-  if (ps2x.Button(PSB_START))        // Для перезапуска связи с контроллером
+  ps2x.read_gamepad(false, vibrate); // Читаем контроллер и установаем вибро-мотор
+  if (ps2x.Button(PSB_START))        // Перезапускает связь с контроллером
     Serial.println("Start is being held");
   if (ps2x.Button(PSB_SELECT))
     Serial.println("Select is being held");
@@ -61,11 +60,14 @@ void loop() {
     delay(300);
     ps2x.read_gamepad(false, 0);
   }
+  // Это установит силу вибро-мотора в зависимости от того, насколько сильно вы нажимаете синюю кнопку (X)
+  vibrate = ps2x.Analog(PSAB_CROSS);
 
   if (ps2x.NewButtonState()) {
-    // Нажатие самых нижних кнопок с торца. Включение соленоида
-    if (ps2x.Button(PSB_L2) || ps2x.Button(PSB_R2))
-      { Serial.println("L2 pressed"); digitalWrite(5, HIGH); } else { digitalWrite(5, LOW); }
+    // Нажатие кнопки пульта для выполнения соленоида
+    if (ps2x.Button(PSB_L2) || ps2x.Button(PSB_R2)) { Serial.println("L2 pressed"); digitalWrite(actuator, HIGH); }
+    else { digitalWrite(actuator, LOW); }
+  }
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   // Управление джойстиками
   // Левый джойстик
@@ -79,12 +81,12 @@ void loop() {
     DCMotor_3->run(FORWARD);
     Serial.println(ps2x.Analog(PSS_LY), DEC);
   }
-  else {
+  else { // Останов двигателя
     DCMotor_3->setSpeed(0);
     DCMotor_3->run(RELEASE);
 
   }
-  // Правый джойстик (Можно попробовать использовать для левого, только заменить на LX)
+  // Правый джойстик
   if (ps2x.Analog(PSS_RY) > 130) {
     DCMotor_1->setSpeed((map(ps2x.Analog(PSS_RY), 130, 255, 30, 255)));
     DCMotor_1->run(BACKWARD);
@@ -94,7 +96,7 @@ void loop() {
     DCMotor_1->setSpeed((map(ps2x.Analog(PSS_RY), 125, 0, 30, 255)));
     DCMotor_1->run(FORWARD);
   }
-  else { // Останов двигателей
+  else { // Останов двигателя
     DCMotor_1->setSpeed(0);
     DCMotor_1->run(RELEASE);
   }
